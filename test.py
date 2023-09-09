b934d70289ce630c6df94d31f21b6d3a6748ad1b
prashantmishra311 <prashantmishra311@gmail.com>
2023-09-10 00:53:18 +0530

    modified the class with docstrings

--- test.py
+++ test.py
@@ -2,14 +2,26 @@
 from typing import List, Set, Optional, Callable
 
 class TestClass(object):
-
+    """Class to generate a gmail id for a person
+    """    
     def __init__(self, first_name:str, last_name:str, middle_name:Optional[str]=None):
+        """method to initialize `TestClass`
 
+        Args:
+            first_name (str): first name of the person
+            last_name (str): last name of the person
+            middle_name (Optional[str], optional): middle name of the person. Defaults to None.
+        """        
         self.first_name = first_name
         self.last_name = last_name
         self.middle_name = middle_name
 
     def generate_mail(self):
+        """method to generate mail id
+
+        Returns:
+            str: gmail id of person
+        """        
 
         if self.middle_name:
             return f"{self.first_name.lower()}.{self.middle_name.lower()}.{self.last_name}@gmail.com"
